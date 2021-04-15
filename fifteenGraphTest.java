aa82cf25fd400f0df4229b169d22b7ac060517f5
Austin Laurin <Aglaurin8606@gmail.com>
2021-04-15 00:32:53 -0400

    junit now working

--- fifteenGraphTest.java
+++ fifteenGraphTest.java
@@ -3,11 +3,5 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class fifteenGraphTest {
-	@Test
-    void testNodeGetDepth() {
-        Node n = new Node(null, 1);
-        int expected = 1;
-        int actual = n.getDepth();
-        assertEquals(expected, actual);
-    }
+
 }
