0406e40fd1b27377c8cfef09a63ea072147a47b9
Austin Laurin <Aglaurin8606@gmail.com>
2021-04-14 23:41:19 -0400

    added node constructor and field getters

--- fifteenGraph.java
+++ fifteenGraph.java
@@ -1,6 +1,19 @@
 public class fifteenGraph {
     private static class Node {
-        int[][] element;
-        int depth;
+        private int[][] element;
+        private int depth;
+
+        Node(int[][] element, int depth) {
+            this.element = element;
+            this.depth = depth;
+        }
+
+        public int[][] getElement() {
+            return element;
+        }
+
+        public int getDepth() {
+            return depth;
+        }
     }
 }
