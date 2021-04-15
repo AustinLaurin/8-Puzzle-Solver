1dbca26a1db36534e9e46a5f75ea074e3e1f216a
Austin Laurin <Aglaurin8606@gmail.com>
2021-04-15 09:54:11 -0400

    added method to calculate distances from final position; begun working on calculating number of linear conflicts

--- fifteenGraph.java
+++ fifteenGraph.java
@@ -2,10 +2,12 @@ public class fifteenGraph {
     private static class Node {
         private int[][] element;
         private int depth;
+        private int distancesFromFinalPositionSum;
 
         Node(int[][] element, int depth) {
             this.element = element;
             this.depth = depth;
+            distancesFromFinalPositionSum = calculateDistancesFromFinalPosition();
         }
 
         public int[][] getElement() {
@@ -14,6 +16,45 @@ public class fifteenGraph {
 
         public int getDepth() {
             return depth;
+        }
+
+        public int calculateDistancesFromFinalPosition() {
+            int sum = 0;
+            
+            for(int i = 0; i < element.length; i++) {
+                for(int j = 0; j < element[i].length; j++) {
+                    if(element[i][j] == 0)
+                        break;
+                    int iExpected = (element[i][j] - 1) / element[i].length;
+                    int jExpected = (element[i][j] - 1) % element[i].length;
+
+                    sum = Math.abs(i - iExpected) + Math.abs(j - jExpected);
+                }
+            }
+            
+            return sum;
+        }
+
+        public int getDistancesFromFinalPosition() {
+            return distancesFromFinalPositionSum;
+        }
+
+        public int calculateLinearConflicts() {
+            return caclculateHorizontalLinearConflicts() + calculateVerticalLinearConflicts();
+        }
+
+        private int calculateVerticalLinearConflicts() {
+            int count = 0;
+
+            
+
+            return count;
+        }
+
+        private int caclculateHorizontalLinearConflicts() {
+            int count = 0;
+
+            return count;
         }
     }
 
@@ -29,7 +70,6 @@ public class fifteenGraph {
     }
 
     public int h(Node n) {
-        
-        return 0;
+        return n.getDistancesFromFinalPosition();
     }
 }
