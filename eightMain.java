2bf77721cc7ccce71a47938bc92374676694261d
Austin Laurin <Aglaurin8606@gmail.com>
2021-04-18 15:25:17 -0400

    added file for puzzle input and executable for convenience; file will help avoid recompiling for new puzzle inputs

--- eightMain.java
+++ eightMain.java
@@ -1,12 +1,30 @@
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class eightMain {
     public static void main(String[] args) {
-        int[][] puzzle = {{4, 2, 5},
-                          {1, 0, 6},
-                          {3, 8, 7}};
-                          
-        eightGraph eg = new eightGraph(puzzle);
-        eg.solve();
-        System.out.println("Moves made: ");
-        eg.listMoves();
+        // int[][] puzzle = {{4, 2, 5},
+        //                   {1, 0, 6},
+        //                   {3, 8, 7}};
+        try {
+            FileReader fr = new FileReader("puzzle");
+            int[][] puzzle = new int[3][3];
+            Scanner s = new Scanner(fr);
+
+            for(int i = 0; i < puzzle.length; i++) {
+                for(int j = 0; j < puzzle[i].length; j++) {
+                    if(s.hasNextInt())
+                        puzzle[i][j] = s.nextInt();
+                }
+            }
+
+            eightGraph eg = new eightGraph(puzzle);
+            eg.solve();
+            System.out.println("Moves made: ");
+            eg.listMoves();
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
